c226fd7e4af1283e0a9a920ea7c3690cd118ac57
Omar Mohamed El-Zanaty <omaromar123extra@gmail.com>
2025-05-03 10:47:51 +0300

    handle the upload for the images

--- backend/src/routes/api/upload.ts
+++ backend/src/routes/api/upload.ts
@@ -1,24 +1,36 @@
 import express from 'express';
 import multer from 'multer';
 import path from 'path';
+import fs from 'fs';
 const setUpMulterStorage = multer.diskStorage({
-  destination:function(req,file,cb){
-    cb(null,path.resolve(__dirname,'../../../images'))
+  destination: function (req, file, cb) {
+    cb(null, path.resolve(__dirname, '../../../images'));
   },
   filename: function (req, file, cb) {
-    cb(null,file.originalname);
-  }
-})
-const uploadRouter= express.Router();
+    cb(null, file.originalname);
+  },
+  
+});
+// intialize the router
+const uploadRouter = express.Router();
 
-const upload = multer({storage:setUpMulterStorage});
-uploadRouter.post('/', upload.single('hello'),(req, res) => {
+const upload = multer({ storage: setUpMulterStorage });
+uploadRouter.post('/', upload.single('hello'), (req, res) => {
   try {
-    res.json({ message: 'image successfuly uploaded', file: req.file });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: 'Image upload failed' });
+    const width = parseInt(req.query.width as string);
+    const height = parseInt(req.query.height as string);
+
+    if (isNaN(width) || isNaN(height) || width <= 0 || height <= 0) {
+      res.status(400).send('Invalid width or height. Both must be positive integers.');
+      return;
+    }
+    res.send(`Your image is available at: http://localhost:3000/api/resizedImages?name=${encodeURIComponent(req.file?.originalname as string)}&width=${width}&height=${height}`);
+    return;
+
+  } catch (err) {
+    res.status(500).send('An error occurred while processing the upload.');
   }
-})
+});
 
 export default uploadRouter;
+ 
